173f9e711ed2ce91b1df71d6294bc1503ce5282a
bakhah <franck.hourdin@pictotravel.fr>
2016-10-06 17:01:08 +0200

    Fix calendar size

--- web/calendar/show.php
+++ web/calendar/show.php
@@ -1,3 +1,4 @@
+
 <?php
 include __DIR__.'/../partial/header.php';
 include __DIR__.'/../authentification/startSession.php';
@@ -5,11 +6,16 @@ require('SimpleCalendar.php');
 
 $date = date("m-Y");
 
-echo "<h2>".$date."</h2>";
-$Calendar = new donatj\SimpleCalendar($date);
-$Calendar->addDailyHtml("Midi : Mon cul", "14 October 2016");
-$Calendar->addDailyHtml("Soir : Ma bite", "14 October 2016");
+
+$Calendar = new donatj\SimpleCalendar("October 2016");
+$Calendar->addDailyHtml("Midi : Tagliatelles bolognaise", "11-10-2016");
+$Calendar->addDailyHtml("Soir : Carpaccio de boeuf Frites", "11-10-2016");
+
+echo "<h1>".$date."</h1>";
+echo "<div class=\"container\">";
+echo "<div class=\"table-responsive\">";
 $Calendar->show();
+echo "</div></div>";
 
 include __DIR__.'/../partial/footer.php';
 ?>
