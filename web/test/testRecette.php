6325353a75befd44c31845305e25123a7c8b5be6
Louis <louis.lalleau@gmail.com>
2016-10-13 19:30:22 +0200

    Revert "amend recette"
    This reverts commit ad3aeafe9a3a9dd46d6b8966f53a5ec2fd64f6de.

--- web/test/testRecette.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $data = array(
-    'id'=>1,
-    'name'=>"Omelette",
-    'instruction'=>"<ol type='1'><li>Casser les oeufs</li><li>Battre les oeufs</li><li>Cuire les oeufs</li></ol>",
-    'id_user'=>0,
-  );
-  print_r($data);
-?>
