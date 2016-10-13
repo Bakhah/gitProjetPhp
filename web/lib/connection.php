6f9b8f5f4f54f20b73aadfac31604b00df4057e5
Louis <louis.lalleau@gmail.com>
2016-10-13 19:30:22 +0200

    Revert "amend recette"
    This reverts commit ad3aeafe9a3a9dd46d6b8966f53a5ec2fd64f6de.

--- web/lib/connection.php
+++ web/lib/connection.php
@@ -1,8 +1,6 @@
 <?php
     $g_link = false;
-    /**
-    * Ouvre une connection avec la base de donnée du projet
-    */
+
     function GetMyConnection()
     {
         global $g_link;
@@ -12,9 +10,7 @@
         mysqli_select_db($g_link, 'PHP_PROJECT') or die('Could not select database.');
         return $g_link;
     }
-    /**
-    * Ferme la connection avec la base de donnée
-    */
+
     function CleanUpDB()
     {
         global $g_link;
@@ -23,13 +19,4 @@
         $g_link = false;
     }
 
-    /**
-    * Envoie une requête à la base
-    * @param requête sql
-    * @return le resultat de cette requete sans fetch ou false
-    */
-    function request($sql){
-      return mysqli_query($con, $sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysqli_error());
-    }
-    $con = GetMyConnection();
 ?>
